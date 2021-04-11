b1b02ea86a75d7b6725ab346cde3206ca8198475
Alexandr Krasnov <sanor00@yandex.ru>
2021-04-11 09:41:50 +0300

    initial commit

---
+++ VendingMachineEscape/Assets/Scripts/Movement/PlayerMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+  public CharacterController controller;
+  public float speed = 6f;
+  void Start() {
+    controller = gameObject.GetComponent<CharacterController>();
+  }
+
+  // Update is called once per frame
+  void Update() {
+    float horizontal = Input.GetAxisRaw("Horizontal");
+    float vertical = Input.GetAxisRaw("Vertical");
+    Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+
+    if (direction.magnitude >= 0.1f) {
+      float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
+      transform.rotation = Quaternion.Euler(0f, targetAngle, 0f);
+      controller.Move(direction * speed * Time.deltaTime);
+
+      if (direction != Vector3.zero) {
+        gameObject.transform.forward = direction;
+      }
+    }
+  }
+}
